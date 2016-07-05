8669837b5fbc5e36b44a122139576e892406fa1b
Megtev <illiasmoliarchuk@icloud.com>
2016-07-05 09:05:36 +0300

    Update Learning.cpp

--- Lesson13-1/Learning.cpp
+++ Lesson13-1/Learning.cpp
@@ -8,8 +8,28 @@
 #include "Stroustrup\Window.cpp"
 #include "Stroustrup\std_lib_facilities.h"
 
-//namespace Graph_lib {
-//}
+/*namespace Graph_lib {
+void Striped_rectangle::draw_lines() const
+    {
+    if (fill_color().visibility()) {    // fill
+        fl_color(fill_color().as_int());
+               int x = 2;
+        while(true) {
+                if(point(0).y + x >= point(0).y + h) 
+                    break;
+                fl_line(point(0).x, point(0).y + x, point(0).x + w, point(0).y + x);
+                x += 2;
+                }
+        fl_color(color().as_int());    // reset color
+    }
+
+    if (color().visibility()) {    // edge on top of fill
+        fl_color(color().as_int());
+        fl_rect(point(0).x,point(0).y,w,h);
+    }
+    }
+
+*/}
 
 
 int main() {
